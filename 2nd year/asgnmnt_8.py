5c1e081e9fa7725c596b148be2eb988158acf434
harrymunot1311 <harrymunot1311@gmail.com>
2026-09-06 17:12:04 +0530

    Add files via upload

---
+++ 2nd year/asgnmnt_8.py
@@ -0,0 +1,12 @@
+with open("file.txt", "r") as file:
+    lines = file.readlines()
+
+print("Total lines:", len(lines))
+
+# first two lines
+first_two = lines[:2]
+
+with open("output.txt", "w") as file:
+    file.writelines(first_two)
+
+print("First two lines written to output.txt")
